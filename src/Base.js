55efc6e4851790248881a9819be5a24578129b8e
holuwabamidele <yemo4real@ymail.com>
2024-08-06 15:14:39 +0100

    furniture

--- src/Base.js
+++ src/Base.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const app = initializeApp({
   apiKey: "AIzaSyDLfENG0WjMKv0aDDgvGHPE6_LBSwuGVuE",
@@ -10,5 +11,6 @@ const app = initializeApp({
   appId: "1:98620094826:web:0c0512afe4452043028d13",
 });
 
-const db = getFirestore(app);
-export { db };
+const basedb = getFirestore(app);
+const baseStorage = getStorage(app);
+export { basedb, baseStorage };
--- src/Pages/NewEntry.jsx
+++ src/Pages/NewEntry.jsx
@@ -1,17 +1,52 @@
+import { addDoc, collection } from "firebase/firestore";
+import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
+import { useState } from "react";
 import styled from "styled-components";
+import { basedb, baseStorage } from "../Base";
 
 const NewEntry = () => {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [avatar, setAvatar] = useState("");
+
+  const uploadImage = (e) => {
+    const file = e.target.files[0];
+    const fileRef = ref(baseStorage, "/myImage", +file.name);
+    const storageRef = uploadBytesResumable(fileRef, file);
+    getDownloadURL(storageRef.snapshot.ref).then((url) => {
+      setAvatar(url);
+    });
+  };
+
+  const postData = async () => {
+    addDoc(collection(basedb, "furniture"), {
+      title,
+      description,
+      avatar: await avatar,
+    });
+    alert("Data Submitted");
+  };
+
   return (
     <Container>
       <Wrapper>
         <Link to="/new">
-          <input type="file" />
+          <input type="file" onChange={uploadImage} />
           <br />
-          <input type="Title" placeholder="Item Title" />
+          <input
+            type="Title"
+            placeholder="Item Title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
           <br />
-          <textarea placeholder="Enter Item Description"></textarea>
+          <textarea
+            placeholder="Enter Item Description"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          ></textarea>
           <br />
-          <button>SUBMIT</button>
+          <button onClick={postData}>SUBMIT</button>
         </Link>
       </Wrapper>
     </Container>
